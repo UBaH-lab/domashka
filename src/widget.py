1385409a5cdd75f0194250df5322a9b2ff05bb6f
Иван Бакулин <ubahyska1992@gmail.com>
2025-05-03 00:18:51 +0300

    mask account cards

--- src/widget.py
+++ src/widget.py
@@ -1,3 +1,28 @@
 
-def mask_account_card(card_type_and_number:str)->str:
-  """Принимает строку, содержащую тип и номер карты или счета."""
+from src import masks
+from src.masks import get_mask_card_number
+from datetime import datetime
+
+def mask_account_card(info_card: str) -> str:
+  """Функция определяет счет это или номер карты и возвращает замаскированный"""
+  info_card_list = info_card.split()
+  if "Счет" in info_card_list:
+    print(f"Счет **{info_card_list[-1][-4:]}")
+  else:
+    card_name = ' '.join(info_card_list[:-1])
+    card_number = info_card_list[-1].replace(' ', '')
+    print(f"{card_name} {get_mask_card_number(card_number)}")
+
+card_type_and_number = "Visa Gold 5999414228426353"
+mask_account_card(card_type_and_number)
+
+
+def get_date(date_inp: str) -> str:
+  """Функция, которая принимает на вход строку и возвращает строку с датой."""
+  date = datetime.strptime(date_inp[:10], '%Y-%m-%d')
+  return f"{date.day:02}.{date.month:02}.{date.year}"
+
+
+print(get_date("2024-03-11T02:26:18.671407"))
+
+
